03c238683682d46a437fa873ca4e1d87aee51eb3
log101 <frknerdm@mail.com>
2022-10-13 13:27:05 +0300

    Return checkout form data to frontend

--- src/services/manual-payment.js
+++ src/services/manual-payment.js
@@ -26,7 +26,92 @@ class ManualPaymentService extends PaymentService {
    * @return {object} an object with staus
    */
   async createPayment(cart) {
-    return { cart: cart, status: "pending" }
+    const request = {
+      locale: Iyzipay.LOCALE.TR,
+      conversationId: "123456789",
+      price: "1",
+      paidPrice: "1.2",
+      currency: Iyzipay.CURRENCY.TRY,
+      basketId: "B67832",
+      paymentGroup: Iyzipay.PAYMENT_GROUP.LISTING,
+      callbackUrl: "https://www.merchant.com/callback",
+      enabledInstallments: [2, 3, 6, 9],
+      buyer: {
+        id: "BY789",
+        name: "John",
+        surname: "Doe",
+        gsmNumber: "+905350000000",
+        email: "email@email.com",
+        identityNumber: "74300864791",
+        lastLoginDate: "2015-10-05 12:43:35",
+        registrationDate: "2013-04-21 15:12:09",
+        registrationAddress:
+          "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+        ip: "85.34.78.112",
+        city: "Istanbul",
+        country: "Turkey",
+        zipCode: "34732",
+      },
+      shippingAddress: {
+        contactName: "Jane Doe",
+        city: "Istanbul",
+        country: "Turkey",
+        address: "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+        zipCode: "34742",
+      },
+      billingAddress: {
+        contactName: "Jane Doe",
+        city: "Istanbul",
+        country: "Turkey",
+        address: "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
+        zipCode: "34742",
+      },
+      basketItems: [
+        {
+          id: "BI101",
+          name: "Binocular",
+          category1: "Collectibles",
+          category2: "Accessories",
+          itemType: Iyzipay.BASKET_ITEM_TYPE.PHYSICAL,
+          price: "0.3",
+        },
+        {
+          id: "BI102",
+          name: "Game code",
+          category1: "Game",
+          category2: "Online Game Items",
+          itemType: Iyzipay.BASKET_ITEM_TYPE.VIRTUAL,
+          price: "0.5",
+        },
+        {
+          id: "BI103",
+          name: "Usb",
+          category1: "Electronics",
+          category2: "Usb / Cable",
+          itemType: Iyzipay.BASKET_ITEM_TYPE.PHYSICAL,
+          price: "0.2",
+        },
+      ],
+    }
+
+    const getFormPromise = () =>
+      new Promise((resolve, reject) => {
+        this.iyzipay.checkoutFormInitialize.create(request, (err, result) => {
+          resolve(result.checkoutFormContent)
+        })
+      })
+
+    const form = await getFormPromise()
+
+    return {
+      cart: {
+        ...cart,
+        metadata: {
+          htmlForm: form,
+        },
+      },
+      status: "pending",
+    }
   }
 
   /**
@@ -46,7 +131,23 @@ class ManualPaymentService extends PaymentService {
    * @return {Promise<{ status: string, data: object }>} result with data and status
    */
   async authorizePayment(session, context) {
-    return { status: "authorized", data: { status: "authorized" } }
+    const status = this.getStatus(session)
+    if (status == PaymentSessionStatus.PENDING) {
+      console.log("\n\n\n\n\nSETTING TO REQUIRES MORE")
+      return {
+        status: PaymentSessionStatus.REQUIRES_MORE,
+      }
+    } else if (status == PaymentSessionStatus.REQUIRES_MORE) {
+      console.log("\n\n\n\n\nNOW DONE")
+      return {
+        status: "authorized",
+        data: { status: "authorized" },
+      }
+    } else {
+      return {
+        status: PaymentSessionStatus.ERROR,
+      }
+    }
   }
 
   /**
@@ -55,6 +156,14 @@ class ManualPaymentService extends PaymentService {
    * @return {object} same data
    */
   async updatePayment(sessionData) {
+    /* if (sessionData.data.action === "get-payment-form") {
+      return {
+        ...sessionData,
+        paymentForm: "<html></html>",
+      }
+    } else {
+      return sessionData.data
+    }*/
     return sessionData.data
   }
 
